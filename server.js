90af0e18b2d17f9acb6e266244a8d69c62fd2055
Saikumar <sai@errsole.com>
2024-04-23 18:12:45 +0530

    refactor

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
 const errsole = require('errsole');
 const ErrsoleMongoDB = require('errsole-mongodb');
-// Initialize errsole with ErrsoleMongoDB as the storage backen
 errsole.initialize({
-  storage: new ErrsoleMongoDB('mongodb+srv://saikumar:rishi9100@cluster0.mcal1by.mongodb.net/', 'deployDatabase'),
-  enableDashboard: false
+  storage: new ErrsoleMongoDB('mongodb+srv://saikumar:rishi9100@cluster0.mcal1by.mongodb.net/', 'normalDB')
 });
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
 
-// Error handling middleware
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello World!');
 });
+
+app.use('/api', createProxyMiddleware({
+  target: 'http://localhost:8001',
+  changeOrigin: true
+}));
 
 app.get('/logs', (req, res) => {
-  console.log('console log statement');
-  console.log('error statment ');
+  console.log('normal log statment');
+  console.error('error log statment');
+  console.warn('warn log statment');
+  console.info('info log statment');
+  console.debug('debug log statment');
+  res.send('logs are generated and saved in database');
 });
-
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
 });
